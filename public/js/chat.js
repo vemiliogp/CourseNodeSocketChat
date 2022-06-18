f50f5bab321b1f372052d29fd1426ab89eb62b22
Victor E Gonzalez P <vemiliogp@gmail.com>
2022-06-18 16:25:46 +0200

    Initial commit

---
+++ public/js/chat.js
@@ -0,0 +1,127 @@
+const url = window.location.hostname.includes("localhost")
+  ? "http://localhost:8080/api/auth/"
+  : "https://rest-server-vegp.herokuapp.com/api/auth/";
+
+let usuario = null;
+let socket = null;
+
+// Referencias HTML
+
+const txtUid = document.querySelector("#txtUid");
+const txtMensaje = document.querySelector("#txtMensaje");
+const ulUsuarios = document.querySelector("#ulUsuarios");
+const ulMensajes = document.querySelector("#ulMensajes");
+const btnSalir = document.querySelector("#btnSalir");
+
+const validarJWT = async () => {
+  const token = localStorage.getItem("token") || "";
+
+  if (token.length <= 10) {
+    window.location = "index.html";
+    throw new Error("No hay token en el servidor");
+  }
+
+  const resp = await fetch(url, { headers: { "x-token": token } });
+  const { usuario: userDB, token: tokenDB } = await resp.json();
+
+  usuario = userDB;
+
+  document.title = usuario.nombre;
+
+  await conectarSocket();
+};
+
+const conectarSocket = () => {
+  socket = io({
+    extraHeaders: {
+      "x-token": localStorage.getItem("token"),
+    },
+  });
+
+  socket.on("connect", () => {
+    console.log("Sockets online");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Sockets offline");
+  });
+
+  socket.on("recibir-mensajes", dibujarMensajes);
+
+  socket.on("usuarios-activos", dibujarUsuarios);
+
+  socket.on("mensaje-privado", (payload) => {
+    console.log("Mensaje privado:", payload);
+  });
+};
+
+const dibujarUsuarios = (usuarios = []) => {
+  let usersHtml = "";
+  usuarios.forEach(({ uid, nombre }) => {
+    usersHtml += `
+      <li>
+        <p>
+          <h5 class="text-success">${nombre}</h5>
+          <span class="fs-6 text-muted">${uid}</span>
+        </p>
+      </li>
+    `;
+  });
+  ulUsuarios.innerHTML = usersHtml;
+};
+
+const dibujarMensajes = (mensajes = []) => {
+  let mensajesHtml = "";
+  mensajes.forEach(({ nombre, mensaje }) => {
+    mensajesHtml += `
+      <li>
+        <p>
+          <span class="text-primary">${nombre}</span>
+          <span>${mensaje}</span>
+        </p>
+      </li>
+    `;
+  });
+  ulMensajes.innerHTML = mensajesHtml;
+};
+
+txtMensaje.addEventListener("keyup", ({ keyCode }) => {
+  const mensaje = txtMensaje.value;
+  const uid = txtUid.value;
+
+  if (keyCode !== 13) {
+    return;
+  }
+
+  if (mensaje.length === 0) {
+    return;
+  }
+
+  socket.emit("enviar-mensaje", { mensaje, uid });
+
+  txtMensaje.value = "";
+});
+
+btnSalir.addEventListener("click", () => {
+  localStorage.removeItem("token");
+
+  const auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(() => {
+    console.log("User signed out.");
+    window.location = "index.html";
+  });
+});
+
+const main = async () => {
+  // Validar JWT
+  await validarJWT();
+};
+
+(() => {
+  gapi.load("auth2", () => {
+    gapi.auth2.init();
+    main();
+  });
+})();
+
+// main();
